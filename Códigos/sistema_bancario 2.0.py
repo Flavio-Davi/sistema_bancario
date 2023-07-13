e43604a4912962abd31f0ea9e89fae6bc46c8029
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2023-07-13 00:23:21 -0300

    Update sistema_bancario 2.0.py
    Atualização de código, pois o mesmo havia sido upado com a versão anterior.

--- Códigos/sistema_bancario 2.0.py
+++ Códigos/sistema_bancario 2.0.py
@@ -1,54 +1,194 @@
 from datetime import datetime
+from os import system
+from time import sleep
 
-menu = """
 
-[d] - Depositar
-[s] - Saque
-[e] - Extrato
-[q] - Sair
-
-=> """
-
+cores = {red := "\033[31m", green := "\033[32m", yellow := "\033[33m", blue := "\033[34m", purple := "\033[35m", cyano := "\033[1;36m", endcor := "\033[m"}
 
 dia = datetime.today().day
 mes = datetime.today().month
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
 
-while True:
 
-        opcao = input(menu).lower().split()[0]
+def menu():
 
-        if opcao == "d":
-            deposito = float(input("Digite o valor de depósito: "))
-            if deposito < 1:
-                print("Valor inferior a R$1,00")
-            else:
-                saldo += deposito
-                extrato += f"R${deposito:.2f} depositado dia {dia}/{mes}\n"
-                print(f"Valor de R${deposito:.2f} depositado com sucesso.")
+    while True:
+        print(cyano+"-"*25, f"\n{'MENU':^25}")
+        print("-"*25)
+        menu = input("""[ D ]\tDepósito
+[ S ]\tSaque
+[ E ]\tExtrato
+[ C ]\tCriar usuário
+[ N ]\tCriar conta
+[ L ]\tListar contas
+[ F ]\tSair\n➥ """).lower().split()[0]
+    
+        if menu != "d" and menu != "s" and menu != "e" and menu != "f" and menu != "c" and menu != "n" and menu != "l":
+            print(red + "ERROR: Opção inválida" + endcor)
+            sleep(2)
+            system("cls")
+            continue
+        else:
+            return menu
 
-        elif opcao == "s":
-            saque = float(input("Digite o valor que deseja sacar: "))
-            if saque > 500:
-                 print("Valor solicitado superior ao permitido.")
-            elif saldo >= saque and LIMITE_SAQUES > 0:
-                saldo -= saque
-                print(f"Saque de R${saque} realizado com sucesso.")
-                extrato += f"R${saque} sacado dia{dia}/{mes}\n"
-                LIMITE_SAQUES -= 1
-            else:
-                print("Limite de saques diário excedido, volte amanhã.")
 
-        elif opcao == "e":
-            print("Extrato\n\n")
-            print(extrato)
+def deposito(saldo, extrato, valor_deposito=float, /):
+        
+        if valor_deposito < 2:
+            print("Operação falhou, valor inferior a R$2,00")
+        
+        else:
+            saldo += valor_deposito
+            extrato += f"Deposito R${f'{valor_deposito:.2f}':<10}{dia:>13}/{mes}\n"
+            print(green + f"R${valor_deposito:.2f} depositado com sucesso.", endcor)
+        
+        return saldo, extrato 
 
-        elif opcao == "q":
+
+def saque(*, saldo, extrato, num_saques=0, valor_saque=float):
+
+    LIMITE_SAQUE_DIARIO = 3
+    limite_saque = 500
+
+    if valor_saque > saldo:
+        print(red+"Operação falhou, saldo insuficiente", endcor)
+
+    elif valor_saque < 2:
+        print(red+ "Operação falhou, valor a baixo do mínimo de R$2,00", endcor)
+    
+    elif valor_saque > limite_saque:
+        print(red+"Operação falhou, valor solicitado excede limite de saque único", endcor)
+
+    elif LIMITE_SAQUE_DIARIO == num_saques:
+        print(red+"Operação falhou, limite de saque diário excedido", endcor)
+    
+    else:
+        saldo -= valor_saque
+        num_saques += 1
+        extrato += f"Saque R${f'{valor_saque:.2f}':<10}{dia:>16}/{mes}\n"
+        print(green + f"R${valor_saque:.2f} sacado com sucesso" + endcor)
+
+    return saldo, extrato, num_saques
+
+
+def func_extrato(saldo, extrato):
+        print(yellow+"Não foram realizado movimentações." if not extrato else yellow+f"{'-'*36}\nInformações\t\t\tData\n{'-'*36}\n"+extrato)
+        print(green+f"\nSaldo Total: {saldo}")
+        print(yellow+"-"*36)
+
+
+def criar_usuario(users=list):
+
+    dados_user = {"Nome": None, "Nascimento": None, "CPF": None, "Endereço": None}
+
+    
+    dados_user["Nome"] = input("Nome: ")
+    dados_user["CPF"] = input("CPF: ")
+    for c in range(len(users)):
+        if users[c] == dados_user["CPF"]:
+            return print(yellow+"CPF já cadastrado no sistema")
+        else:
+            continue
+
+    dados_user["Nascimento"] = {"Dia": "", "Mês": "", "Ano": ""}
+    print("Data de nascimento")
+    for key, value in dados_user["Nascimento"].items():
+        dados_user["Nascimento"][key] = input(f"\t{key}: ")
+    
+    dados_user["Endereço"] = {"Logradouro": "", "Bairro": "", "Cidade": "", "Estado": ""}
+    print("Endereço")
+    for key, value in dados_user["Endereço"].items():
+        dados_user["Endereço"][key] = input(f"\t{key}: ")
+    
+    while True:
+        if len(dados_user["CPF"]) != 11 or dados_user["CPF"].isnumeric() == False:
+            system("cls")
+            print(red+"ERROR: CPF inválido", endcor)
+            sleep(2)
+            dados_user["CPF"] = str(input("CPF: "))
+        else:
+            break 
+    users.append(dados_user)
+    
+    return users 
+
+
+def criar_conta(CPF, list_users=list):
+    for c in range(len(list_users)):
+        if list_users[c]["CPF"] == CPF:
+            print(green+"Conta criada com sucesso.")
+            return list_users[c]["Nome"]
+        else:
+            continue
+
+    return print(yellow+"CPF não encontrado, volte ao menu e crie um usuario."), sleep(2)
+
+
+def listar_users(contas):
+    print("-"*36)
+    for c in range(len(contas)):
+        for key, value in contas[c].items():
+            print(f"{key:<15}{value:>17}")
+            if key == "Agência":
+                print("\n")
+                continue
+    print("-"*36)
+
+
+def main():
+    saldo = 0
+    num_saques = 0
+    users = list()
+    list_contas = list()
+    num_conta = 0
+    extrato = ""
+
+    while True:
+        home = menu()
+        if home == "d":
+            system("cls")
+            Vdeposito = float(input("Valor de depósito: R$"))
+            saldo, extrato = deposito(saldo, extrato, Vdeposito)
+
+        elif home == "s":
+            system("cls")
+            Vsaque = float(input("Valor de saque: R$"))
+            saldo, extrato, num_saques = saque(saldo=saldo, extrato=extrato, num_saques=num_saques, valor_saque=Vsaque)
+
+        elif home == "e":
+            system("cls")
+            func_extrato(saldo, extrato)
+            input(yellow+"Presiona enter para voltar.")
+            system("cls")
+
+        elif home == "c":
+            system("cls")
+            print("-"*45, f"\n{'DADOS CADASTRAIS':^45}")
+            print("-"*45)
+            users = criar_usuario(users)
+            print(green+"Usuário criado com sucesso!")
+            sleep(2)
+            system("cls")
+        
+        elif home == "n":
+            system("cls")
+            num_conta += 1 
+            cpf = input(green+"Digite seu CPF: ")
+            nome = criar_conta(cpf, users)
+            conta = {"Usuário": nome, "Número da conta": num_conta, "Agência": "001"}
+            list_contas.append(conta)
+            sleep(2)
+            system("cls")
+
+        elif home == "l":
+            system("cls")
+            listar_users(list_contas)
+            input("Pessione enter para voltar")
+            system("cls")
+
+        elif home == "f":
             break
 
-        else:
-             print("Operação inválida, por favor selecione novamente a operação desejada.")
+    return print("Volte sempre")
+
+
+main()
